ead9bb9352cc7adc7a08006c68976fe234bf854d
Adesina <damiiyi@gmail.com>
2024-02-19 14:11:44 +0100

    First commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,126 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Tensfer",
+  tagline: "The safer way for your users to pay with crypto",
+  favicon: "img/icon.webp",
+
+  // Set the production url of your site here
+  url: "https://tensfer.co",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "tensfer", // Usually your GitHub org/user name.
+  projectName: "tensfer-website", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/Tensfer/tensfer-docs/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://medium.com/@tensferHQ/tensfer-connecting-crypto-b2023fc15eff",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: "Tensfer logo",
+        src: "img/logo_colored.webp",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Build",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+      ],
+    },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Quickstart",
+    //           to: "/docs/intro",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "Community",
+    //       items: [
+    //         {
+    //           label: "Stack Overflow",
+    //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
+    //         },
+    //         {
+    //           label: "Discord",
+    //           href: "https://discordapp.com/invite/docusaurus",
+    //         },
+    //         {
+    //           label: "Twitter",
+    //           href: "https://twitter.com/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "More",
+    //       items: [
+    //         {
+    //           label: "Blog",
+    //           to: "/blog",
+    //         },
+    //         {
+    //           label: "GitHub",
+    //           href: "https://github.com/facebook/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
