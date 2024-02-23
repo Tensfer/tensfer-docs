6253b3c4fbfd937641af12003dfc51656c2886c1
Adesina <damiiyi@gmail.com>
2024-02-23 04:36:06 +0100

    added icons to sidebar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,8 +38,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/Tensfer/tensfer-docs/",
+          editUrl: "https://github.com/Tensfer/tensfer-docs/",
         },
         blog: {
           showReadingTime: true,
